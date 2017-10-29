0536f9fb3bcc7d2ea6ff0ec009c57e19fbd181eb
Gabriel Furtado de Almeida <gfurtadoalmeida@gmail.com>
2017-10-29 20:53:09 +0000

    First algorithm: Selection Sort

---
+++ Algorithms/Sorting/SelectionSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Algorithms.Sorting
+{
+    public sealed class SelectionSort<T> : AbstractSort<T> where T : IComparable<T>
+    {
+        public override void Sort(T[] input)
+        {
+            // Find the smallest item and exchange it with the first item.
+            // Goes to next item.
+            // Find the smallest item and exchange it with the second item.
+            // ...
+            // Repeat until end.
+
+            Int32 N = input.Length;
+
+            for (int i = 0; i < N; i++)
+            {
+                Int32 indexMin = i;
+
+                for (int j = i + 1; j < N; j++)
+                {
+                    if (base.IsLess(input[j], input[indexMin]))
+                        indexMin = j;
+                }
+
+                base.Exchange(input, i, indexMin);
+            }
+        }
+    }
+}
---
+++ Executor/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using Algorithms.Sorting;
+
+namespace Executor
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            const Int32 INPUT_SIZE = 25;
+            const Int32 MAX_VALUE = 100;
+
+            Random random = new Random();
+            Int32[] input = new Int32[INPUT_SIZE];
+
+            for (int i = 0; i < input.Length; i++)
+                input[i] = random.Next(MAX_VALUE);
+
+            Describe("Ïnput", input);
+
+            AbstractSort<Int32> algorithm = new SelectionSort<Int32>();
+
+            algorithm.Sort(input);
+
+            Describe("Sorted", input);
+
+            Console.ReadKey();
+        }
+
+        static void Describe(String label, IEnumerable enumerable )
+        {
+            Console.WriteLine(label + ": ");
+
+            IEnumerator enumerator = enumerable.GetEnumerator();
+
+            while (enumerator.MoveNext())
+                Console.Write(" " + enumerator.Current);
+
+            Console.WriteLine();
+        }
+    }
+}
