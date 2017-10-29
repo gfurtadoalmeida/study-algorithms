6e4bd75abf6ca07d6918a4da4afc94b7517ab645
Gabriel Furtado de Almeida <gfurtadoalmeida@gmail.com>
2017-10-29 21:51:10 +0000

    Better explanation

--- Algorithms/Sorting/SelectionSort.cs
+++ Algorithms/Sorting/SelectionSort.cs
@@ -5,20 +5,21 @@ namespace Algorithms.Sorting
     public sealed class SelectionSort<T> : AbstractSort<T> where T : IComparable<T>
     {
         public override void Sort(T[] input)
-        {
-            // Find the smallest item and exchange it with the first item.
-            // Goes to next item.
-            // Find the smallest item and exchange it with the second item.
+        { 
+            // Start at index 0.
+            // Find the smallest item and exchange it with the item on index 0.
+            // Increment index.
             // ...
             // Repeat until end.
 
-            Int32 N = input.Length;
+            Int32 count = input.Length;
 
-            for (int i = 0; i < N; i++)
+            for (int i = 0; i < count; i++)
             {
                 Int32 indexMin = i;
 
-                for (int j = i + 1; j < N; j++)
+                // Find the index of the last smallest item.
+                for (int j = i + 1; j < count; j++)
                 {
                     if (base.IsLess(input[j], input[indexMin]))
                         indexMin = j;
