4d34beb1a5c1af651aa26c0fa376f5816235a0e3
Gabriel Furtado de Almeida <gfurtadoalmeida@gmail.com>
2017-11-05 18:14:42 +0000

    Adding some tests

---
+++ Algorithms.Test/Sorting/ShellSortTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Algorithms.Sorting;
+using Xunit;
+
+namespace Algorithms.Test.Sorting
+{
+    public sealed class ShellSortTest
+    {
+        [Fact]
+        public void Test()
+        {
+            ShellSort<Int32> sort = new ShellSort<Int32>();
+            Int32[] array = SortData.CreateUnsortedArray();
+
+            sort.Sort(array);
+
+            Assert.True(SortData.VerifyArrayIsSorted(array));
+        }
+    }
+}
