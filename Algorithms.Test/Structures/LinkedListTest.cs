5131271d479bf4131cf8409e97b91675a9b47c7d
Gabriel Furtado de Almeida <gfurtadoalmeida@gmail.com>
2017-11-05 19:52:27 +0000

    New data structure: LinkedList

---
+++ Algorithms.Test/Structures/LinkedListTest.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Algorithms.Structures;
+using Xunit;
+
+namespace Algorithms.Test.Structures
+{
+    public sealed class LinkedListTest
+    {
+        [Fact]
+        public void Test_Empty_OnCreation()
+        {
+            LinkedList<Int32> list = new LinkedList<Int32>();
+
+            Assert.True(list.IsEmpty);
+            Assert.Equal(0, list.Count);
+        }
+
+        [Fact]
+        public void Test_Empty_WhenCleaning()
+        {
+            LinkedList<Int32> list = new LinkedList<Int32>();
+            list.Add(2);
+            list.Remove();
+
+            Assert.True(list.IsEmpty);
+            Assert.Equal(0, list.Count);
+        }
+
+        [Fact]
+        public void Test_NotEmpty_WhenPushing()
+        {
+            LinkedList<Int32> list = new LinkedList<Int32>();
+            list.Add(18);
+            list.Add(2);
+
+            Assert.False(list.IsEmpty);
+            Assert.Equal(2, list.Count);
+        }
+
+        [Fact]
+        public void Test_AddRemoveOrder()
+        {
+            LinkedList<Int32> stack = new LinkedList<Int32>();
+            stack.Add(18);
+            stack.Add(2);
+            stack.Add(88);
+
+            Assert.Equal(88, stack.Remove());
+            Assert.Equal(2, stack.Remove());
+            Assert.Equal(18, stack.Remove());
+        }
+
+        [Fact]
+        public void Test_Enumeration_Count()
+        {
+            LinkedList<Int32> list = new LinkedList<Int32>();
+            list.Add(18);
+            list.Add(2);
+
+            Int32 count = 0; 
+            
+            var enumerator = list.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                count++;
+            }
+
+            Assert.Equal(2, count);
+        }
+
+        [Fact]
+        public void Test_Enumeration_Values()
+        {
+            LinkedList<Int32> list = new LinkedList<Int32>();
+            list.Add(2);
+            list.Add(4);
+            list.Add(8);
+
+            Int32 count = 8;
+
+            var enumerator = list.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                Assert.Equal(enumerator.Current, count);
+
+                count /= 2;
+            }
+        }
+
+        [Fact]
+        public void Test_Contains()
+        {
+            LinkedList<Int32> list = new LinkedList<Int32>();
+            list.Add(18);
+            list.Add(2);
+
+            Assert.True(list.Contains(18));
+            Assert.True(list.Contains(2));
+        }
+
+        [Fact]
+        public void Test_NotContains()
+        {
+            LinkedList<Int32> list = new LinkedList<Int32>();
+            list.Add(18);
+            list.Add(2);
+
+            Assert.False(list.Contains(88));
+        }
+    }
+}
---
+++ Algorithms/Structures/LinkedList.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Algorithms.Structures
+{
+    public sealed class LinkedList<T> : System.Collections.Generic.IEnumerable<T>
+    {
+        private Node _head;
+        private Int32 _itensCount;
+
+        public Boolean IsEmpty => this._head == null;
+        public Int32 Count => this._itensCount;
+
+        public void Add(T item)
+        {
+            if (this.IsEmpty)
+                this._head = new Node(item, null);
+            else
+                this._head = new Node(item, this._head);
+
+            this._itensCount++;
+        }
+
+        public T Remove()
+        {
+            if (this.IsEmpty)
+                throw new InvalidOperationException("No more itens to be removed.");
+
+            T item = this._head.Value;
+
+            this._head = this._head.Next;
+            this._itensCount--;
+
+            return item;
+        }
+
+        public Boolean Contains(T item)
+        {
+            Boolean contains = false;
+            Node current = this._head;
+
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                {
+                    contains = true;
+                    break;
+                }
+
+                current = current.Next;
+            }
+
+            return contains;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node current = this._head;
+
+            while (current != null)
+            {
+                yield return current.Value;
+
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private sealed class Node
+        {
+            public T Value;
+            public Node Next;
+
+            public Node()
+            {
+            }
+
+            public Node(T value, Node next)
+            {
+                this.Value = value;
+                this.Next = next;
+            }
+        }
+    }
+}
