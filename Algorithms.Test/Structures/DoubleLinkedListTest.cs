780510fe52f6ca1d3c318adabdf316760a4e1d98
Gabriel Furtado de Almeida <gfurtadoalmeida@gmail.com>
2017-11-05 20:54:41 +0000

    New data structure: DoubleLinkedList

---
+++ Algorithms.Test/Structures/DoubleLinkedListTest.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using Algorithms.Structures;
+using Xunit;
+
+namespace Algorithms.Test.Structures
+{
+    public sealed class DoubleDoubleLinkedListTest
+    {
+        [Fact]
+        public void Test_Empty_OnCreation()
+        {
+            DoubleLinkedList<Int32> list = new DoubleLinkedList<Int32>();
+
+            Assert.True(list.IsEmpty);
+            Assert.Equal(0, list.Count);
+        }
+
+        [Fact]
+        public void Test_Empty_WhenCleaning()
+        {
+            DoubleLinkedList<Int32> list = new DoubleLinkedList<Int32>();
+            list.Add(2);
+            list.Remove();
+
+            Assert.True(list.IsEmpty);
+            Assert.Equal(0, list.Count);
+        }
+
+        [Fact]
+        public void Test_Empty_WhenCleaningCustom()
+        {
+            DoubleLinkedList<Int32> list = new DoubleLinkedList<Int32>();
+            list.Add(2);
+            list.Remove(2);
+
+            Assert.True(list.IsEmpty);
+            Assert.Equal(0, list.Count);
+        }
+
+        [Fact]
+        public void Test_NotEmpty_WhenAdding()
+        {
+            DoubleLinkedList<Int32> list = new DoubleLinkedList<Int32>();
+            list.Add(18);
+            list.Add(2);
+
+            Assert.False(list.IsEmpty);
+            Assert.Equal(2, list.Count);
+        }
+
+        [Fact]
+        public void Test_NotEmpty_WhenAddingAfter()
+        {
+            DoubleLinkedList<Int32> list = new DoubleLinkedList<Int32>();
+            list.Add(18);
+            list.Add(88);
+            list.AddAfter(18, 2);
+
+            Assert.False(list.IsEmpty);
+            Assert.Equal(3, list.Count);
+        }
+
+        [Fact]
+        public void Test_NotEmpty_WhenAddingBefore()
+        {
+            DoubleLinkedList<Int32> list = new DoubleLinkedList<Int32>();
+            list.Add(18);
+            list.Add(88);
+            list.AddBefore(88, 2);
+
+            Assert.False(list.IsEmpty);
+            Assert.Equal(3, list.Count);
+        }
+
+        [Fact]
+        public void Test_AddRemoveOrder()
+        {
+            DoubleLinkedList<Int32> list = new DoubleLinkedList<Int32>();
+            list.Add(18);
+            list.Add(2);
+            list.Add(88);
+
+            Assert.Equal(88, list.Remove());
+            Assert.Equal(2, list.Remove());
+            Assert.Equal(18, list.Remove());
+        }
+
+        [Fact]
+        public void Test_AddRemoveOrder_After()
+        {
+            DoubleLinkedList<Int32> list = new DoubleLinkedList<Int32>();
+            list.Add(18);
+            list.Add(88);
+            list.AddAfter(88, 2);
+
+            Assert.Equal(88, list.Remove());
+            Assert.Equal(2, list.Remove());
+            Assert.Equal(18, list.Remove());
+        }
+
+        [Fact]
+        public void Test_AddRemoveOrder_Before()
+        {
+            DoubleLinkedList<Int32> list = new DoubleLinkedList<Int32>();
+            list.Add(18);
+            list.Add(88);
+            list.AddBefore(18, 2);
+
+            Assert.Equal(88, list.Remove());
+            Assert.Equal(2, list.Remove());
+            Assert.Equal(18, list.Remove());
+        }
+
+        [Fact]
+        public void Test_RemoveOrder_Custom()
+        {
+            DoubleLinkedList<Int32> list = new DoubleLinkedList<Int32>();
+            list.Add(18);
+            list.Add(2);
+            list.Add(88);
+            
+            list.Remove(2);
+
+            Assert.Equal(88, list.Remove());
+            Assert.Equal(18, list.Remove());
+        }
+
+        [Fact]
+        public void Test_Enumeration_Count()
+        {
+            DoubleLinkedList<Int32> list = new DoubleLinkedList<Int32>();
+            list.Add(18);
+            list.Add(2);
+
+            Int32 count = 0;
+
+            var enumerator = list.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                count++;
+            }
+
+            Assert.Equal(2, count);
+        }
+
+        [Fact]
+        public void Test_Enumeration_Values()
+        {
+            DoubleLinkedList<Int32> list = new DoubleLinkedList<Int32>();
+            list.Add(2);
+            list.Add(4);
+            list.Add(8);
+
+            Int32 count = 8;
+
+            var enumerator = list.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                Assert.Equal(enumerator.Current, count);
+
+                count /= 2;
+            }
+        }
+
+        [Fact]
+        public void Test_Contains()
+        {
+            DoubleLinkedList<Int32> list = new DoubleLinkedList<Int32>();
+            list.Add(18);
+            list.Add(2);
+
+            Assert.True(list.Contains(18));
+            Assert.True(list.Contains(2));
+        }
+
+        [Fact]
+        public void Test_NotContains()
+        {
+            DoubleLinkedList<Int32> list = new DoubleLinkedList<Int32>();
+            list.Add(18);
+            list.Add(2);
+
+            Assert.False(list.Contains(88));
+        }
+    }
+}
---
+++ Algorithms/Structures/DoubleLinkedList.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Algorithms.Structures
+{
+    public sealed class DoubleLinkedList<T> : IEnumerable<T>
+    {
+        private Node _head;
+        private Int32 _itensCount;
+
+        public Boolean IsEmpty => this._head == null;
+        public Int32 Count => this._itensCount;
+
+        public void Add(T item)
+        {
+            if (this.IsEmpty)
+                this._head = new Node(item, null, null);
+            else
+            {
+                this._head = new Node(item, null, this._head);
+                this._head.Next.Previous = this._head;
+            }
+
+            this._itensCount++;
+        }
+
+        public void AddAfter(T parentItem, T childItem)
+        {
+            Node parentNode = this.Find(parentItem);
+
+            if (parentNode == null)
+                throw new Exception("Parent item not found.");
+
+            Node childNode = new Node(childItem, parentNode, parentNode.Next);
+
+            if (parentNode.Next != null)
+                parentNode.Next.Previous = childNode;
+
+            parentNode.Next = childNode;
+
+            this._itensCount++;
+        }
+
+        public void AddBefore(T parentItem, T grandParentItem)
+        {
+            Node parentNode = this.Find(parentItem);
+
+            if (parentNode == null)
+                throw new Exception("Parent item not found.");
+
+            Node grandParentNode = new Node(grandParentItem, parentNode.Previous, parentNode);
+
+            if (parentNode.Previous != null)
+                parentNode.Previous.Next = grandParentNode;
+
+            parentNode.Previous = grandParentNode;
+
+            if (parentNode == this._head)
+                this._head = grandParentNode;
+
+            this._itensCount++;
+        }
+
+        public T Remove()
+        {
+            if (this.IsEmpty)
+                throw new InvalidOperationException("No more itens to be removed.");
+
+            T item = this._head.Value;
+
+            this._head = this._head.Next;
+            this._itensCount--;
+
+            return item;
+        }
+
+        public void Remove(T item)
+        {
+            if (!this.IsEmpty)
+            {
+                Node node = this.Find(item);
+
+                if (node != null)
+                {
+                    if (node == this._head)
+                    {
+                        this._head = this._head.Next;
+
+                        if (this._head != null)
+                            this._head.Previous = null;
+                    }
+                    else
+                        node.Previous.Next = node.Next;
+
+                    this._itensCount--;
+                }
+            }
+        }
+
+        public Boolean Contains(T item)
+        {
+            return this.Find(item) != null;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node current = this._head;
+
+            while (current != null)
+            {
+                yield return current.Value;
+
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private Node Find(T item)
+        {
+            Node current = this._head;
+
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                    break;
+
+                current = current.Next;
+            }
+
+            return current;
+        }
+
+        private sealed class Node
+        {
+            public T Value;
+            public Node Previous;
+            public Node Next;
+
+            public Node()
+            {
+            }
+
+            public Node(T value, Node previous, Node next)
+            {
+                this.Value = value;
+                this.Previous = previous;
+                this.Next = next;
+            }
+        }
+    }
+}
