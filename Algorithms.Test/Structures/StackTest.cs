1c8b173b4fbb23912ddaab0f1a89f0050234a5e8
Gabriel Furtado de Almeida <gfurtadoalmeida@gmail.com>
2017-11-05 19:52:46 +0000

    New data structure: Stack

---
+++ Algorithms.Test/Structures/StackTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Algorithms.Structures;
+using Xunit;
+
+namespace Algorithms.Test.Structures
+{
+    public sealed class StackTest
+    {
+        [Fact]
+        public void Test_Empty_OnCreation()
+        {
+            Stack<Int32> stack = new Stack<Int32>();
+
+            Assert.True(stack.IsEmpty);
+            Assert.Equal(0, stack.Count);
+        }
+        
+        [Fact]
+        public void Test_Empty_WhenCleaning()
+        {
+            Stack<Int32> stack = new Stack<Int32>();
+            stack.Push(2);
+            stack.Pop();
+
+            Assert.True(stack.IsEmpty);
+            Assert.Equal(0, stack.Count);
+        }
+
+        [Fact]
+        public void Test_NotEmpty_WhenPushing()
+        {
+            Stack<Int32> stack = new Stack<Int32>();
+            stack.Push(18);
+            stack.Push(2);
+
+            Assert.False(stack.IsEmpty);
+            Assert.Equal(2, stack.Count);
+        }
+
+        [Fact]
+        public void Test_PushPopOrder()
+        {
+            Stack<Int32> stack = new Stack<Int32>();
+            stack.Push(18);
+            stack.Push(2);
+            stack.Push(88);
+
+            Assert.Equal(88, stack.Pop());
+            Assert.Equal(2, stack.Pop());
+            Assert.Equal(18, stack.Pop());
+        }
+    }
+}
---
+++ Algorithms/Structures/Stack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Algorithms.Structures
+{
+    public sealed class Stack<T>
+    {
+        private LinkedList<T> _list = new LinkedList<T>();
+
+        public Boolean IsEmpty => this._list.IsEmpty;
+        public Int32 Count => this._list.Count;
+
+        public void Push(T item)
+        {
+            this._list.Add(item);
+        }
+
+        public T Pop()
+        {
+            return this._list.Remove();
+        }
+    }
+}
