ada636b49999c0db001c188760246282b9d06f66
Gabriel Furtado de Almeida <gfurtadoalmeida@gmail.com>
2017-11-07 22:16:21 +0000

    New data structure: Queue

---
+++ Algorithms.Test/Structures/QueueTask.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Algorithms.Structures;
+using Xunit;
+
+namespace Algorithms.Test.Structures
+{
+    public sealed class QueueTest
+    {
+        [Fact]
+        public void Test_Empty_OnCreation()
+        {
+            Queue<Int32> queue = new Queue<Int32>();
+
+            Assert.True(queue.IsEmpty);
+            Assert.Equal(0, queue.Count);
+        }
+
+        [Fact]
+        public void Test_Empty_WhenCleaning()
+        {
+            Queue<Int32> queue = new Queue<Int32>();
+            queue.Enqueue(2);
+            queue.Dequeue();
+
+            Assert.True(queue.IsEmpty);
+            Assert.Equal(0, queue.Count);
+        }
+
+        [Fact]
+        public void Test_NotEmpty_WhenEnqueueing()
+        {
+            Queue<Int32> queue = new Queue<Int32>();
+            queue.Enqueue(18);
+            queue.Enqueue(2);
+
+            Assert.False(queue.IsEmpty);
+            Assert.Equal(2, queue.Count);
+        }
+
+        [Fact]
+        public void Test_EnqueueDequeueOrder()
+        {
+            Queue<Int32> queue = new Queue<Int32>();
+            queue.Enqueue(18);
+            queue.Enqueue(2);
+            queue.Enqueue(88);
+
+            Assert.Equal(18, queue.Dequeue());
+            Assert.Equal(2, queue.Dequeue());
+            Assert.Equal(88, queue.Dequeue());
+        }
+    }
+}
---
+++ Algorithms/Structures/Queue.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Algorithms.Structures
+{
+    public sealed class Queue<T>
+    {
+        private Node _head;
+        private Node _tail;
+        private Int32 _itensCount;
+
+        public Boolean IsEmpty => this._head == null;
+        public Int32 Count => this._itensCount;
+
+        public void Enqueue(T item)
+        {
+            if (this.IsEmpty)
+            {
+                this._head = new Node(item, null);
+                this._tail = this._head;
+            }
+            else
+            {
+                Node newHead = new Node(item, null);
+
+                this._head.Previous = newHead;
+                this._head = newHead;
+            }
+
+            this._itensCount++;
+        }
+
+        public T Dequeue()
+        {
+            if (this.IsEmpty)
+                throw new InvalidOperationException("No more itens to be dequeued.");
+
+            T item = this._tail.Value;
+
+            this._tail = this._tail.Previous;
+
+            if (this._tail == null)
+                this._head = null;
+
+            this._itensCount--;
+
+            return item;
+        }
+
+        private sealed class Node
+        {
+            public T Value;
+            public Node Previous;
+
+            public Node(T value, Node previous)
+            {
+                this.Value = value;
+                this.Previous = previous;
+            }
+        }
+    }
+}
