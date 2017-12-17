3966f6feb502274fee37d046c07514c383609d8f
gfurtadoalmeida <gfurtadoalmeida@gmail.com>
2017-12-17 17:10:14 +0000

    New search structure: LinearProbingHash

---
+++ Algorithms.Test/Searching/LinearProbingHashTest.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using Algorithms.Searching;
+using Xunit;
+
+namespace Algorithms.Test.Searching
+{
+    public sealed class LinearProbingHashTest
+    {
+        [Fact]
+        public void Test_IsEmpty_Count_OnCreation()
+        {
+            var hash = new LinearProbingHash<Byte, Char>();
+
+            Assert.True(hash.IsEmpty);
+            Assert.Equal(0, hash.Count);
+        }
+
+        [Fact]
+        public void Test_IsEmpty_Count_WhenCleaning()
+        {
+            var hash = this.CreateFullLinearProbingHash();
+
+            Int32 count = hash.Count;
+
+            for (byte i = 1; i <= count; i++)
+                hash.Delete(i);
+
+            Assert.True(hash.IsEmpty);
+            Assert.Equal(0, hash.Count);
+        }
+
+        [Fact]
+        public void Test_NotEmpty_WhenAdding()
+        {
+            var hash = this.CreateFullLinearProbingHash();
+
+            Assert.False(hash.IsEmpty);
+        }
+
+        [Fact]
+        public void Test_Add_Get()
+        {
+            var hash = new LinearProbingHash<Byte, Char>();
+
+            hash.Add(1, 'A');
+
+            Assert.Equal('A', hash.Get(1));
+        }
+
+        [Fact]
+        public void Test_Get_KeyNotFoundException()
+        {
+            var hash = new LinearProbingHash<Byte, Char>();
+
+            Assert.Throws(typeof(KeyNotFoundException), () => hash.Get(100));
+        }
+
+        [Fact]
+        public void Test_Delete()
+        {
+            var hash = new LinearProbingHash<Byte, Char>();
+
+            hash.Add(1, 'A');
+            hash.Delete(1);
+
+            Assert.False(hash.Contains(1));
+        }
+
+        [Fact]
+        public void Test_Contains()
+        {
+            var hash = new LinearProbingHash<Byte, Char>();
+
+            hash.Add(1, 'A');
+            hash.Add(2, 'B');
+
+            Assert.True(hash.Contains(1));
+            Assert.True(hash.Contains(2));
+        }
+
+        [Fact]
+        public void Test_Keys()
+        {
+            var hash = this.CreateFullLinearProbingHash();
+            var keys = hash.Keys().GetEnumerator();
+
+            Int32 count = 1;
+
+            while (keys.MoveNext())
+                Assert.Equal(count++, keys.Current);
+        }
+
+        private LinearProbingHash<Byte, Char> CreateFullLinearProbingHash()
+        {
+            LinearProbingHash<Byte, Char> hash = new LinearProbingHash<Byte, Char>(7);
+            
+            hash.Add(1, 'A');
+            hash.Add(2, 'B');
+            hash.Add(3, 'C');
+            hash.Add(4, 'D');
+            hash.Add(5, 'E');
+            hash.Add(6, 'F');
+
+            return hash;
+        }
+    }
+}
---
+++ Algorithms/Searching/LinearProbingHash.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Searching
+{
+    public sealed class LinearProbingHash<TKey, TValue> : ISymbolTable<TKey, TValue> where TKey : IComparable<TKey>
+    {
+        private Int32 _keysInTable;
+        private Int32 _tableSize;
+        private Object[] _keys;
+        private TValue[] _values;
+
+        public Int32 Count => this._keysInTable;
+
+        public Boolean IsEmpty => this.Count == 0;
+        
+        public LinearProbingHash() : this(16)
+        {
+        }
+
+        public LinearProbingHash(Int32 capacity)
+        {
+            this._keys = new Object[capacity];
+            this._values = new TValue[capacity];
+
+            this._tableSize = capacity;
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (this._keysInTable >= this._tableSize / 2)
+                this.Resize(2 * this._tableSize);
+
+            Int32 i;
+
+            for (i = this.Hash(key); this._keys[i] != null; i = (i + 1) % this._tableSize)
+                if (this._keys[i].Equals(key))
+                {
+                    this._values[i] = value;
+
+                    return;
+                }
+
+            this._keys[i] = key;
+            this._values[i] = value;
+
+            this._keysInTable++;
+        }
+
+        public Boolean Contains(TKey key)
+        {
+            return this.TryGet(key, out _);
+        }
+
+        public void Delete(TKey key)
+        {
+            if (!this.IsEmpty && this.Contains(key))
+            {
+                Int32 hash = this.Hash(key);
+
+                while (!key.Equals(this._keys[hash]))
+                    hash = (hash + 1) % this._tableSize;
+
+                this._keys[hash] = null;
+                this._values[hash] = default(TValue);
+
+                hash = (hash + 1) % this._tableSize;
+
+                while (this._keys[hash] != null)
+                {
+                    Object keyToRedo = this._keys[hash];
+                    TValue valToRedo = this._values[hash];
+
+                    this._keys[hash] = null;
+                    this._values[hash] = default(TValue);
+
+                    this._keysInTable--;
+
+                    this.Add((TKey)keyToRedo, valToRedo);
+
+                    hash = (hash + 1) % this._tableSize;
+                }
+
+                this._keysInTable--;
+
+                if (this._keysInTable > 0 && this._keysInTable == this._tableSize / 8)
+                    this.Resize(this._tableSize / 2);
+            }
+        }
+
+        public TValue Get(TKey key)
+        {
+            if (this.TryGet(key, out TValue value))
+                return value;
+
+            throw new KeyNotFoundException();
+        }
+
+        public IEnumerable<TKey> Keys()
+        {
+            for (int i = 0; i < this._keys.Length; i++)
+                if (this._keys[i] != null)
+                    yield return (TKey)this._keys[i];
+        }
+
+        private Int32 Hash(TKey key)
+        {
+            // 0x7fffffff
+            // equals
+            // 01111111_11111111_11111111_11111111
+            // it will clear the signal bit and use the remaining 31 bits on the calculation.
+
+            return (key.GetHashCode() & 0x7fffffff) % this._tableSize;
+        }
+
+        private void Resize(Int32 size)
+        {
+            LinearProbingHash<TKey, TValue> temp = new LinearProbingHash<TKey, TValue>(size);
+
+            for (int i = 0; i < this._tableSize; i++)
+                if (this._keys[i] != null)
+                    temp.Add((TKey)this._keys[i], this._values[i]);
+
+            this._keys = temp._keys;
+            this._values = temp._values;
+            this._tableSize = temp._tableSize;
+        }
+
+        private Boolean TryGet(TKey key, out TValue value)
+        {
+            for (int i = this.Hash(key); this._keys[i] != null; i = (i + 1) % this._tableSize)
+                if (this._keys[i].Equals(key))
+                {
+                    value = this._values[i];
+
+                    return true;
+                }
+
+            value = default(TValue);
+
+            return false;
+        }
+    }
+}
