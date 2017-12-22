a0e137493535c490226e58b47347297f328f96b5
gfurtadoalmeida <gfurtadoalmeida@gmail.com>
2017-12-22 22:52:26 +0000

    New graph element: CycleChecker

---
+++ Algorithms.Test/Graphs/CycleCheckerTest.cs
@@ -0,0 +1,37 @@
+﻿using Algorithms.Graphs;
+using Xunit;
+
+namespace Algorithms.Test.Graphs
+{
+    public sealed class CycleCheckerTest
+    {
+        [Fact]
+        public void Test_Has_SelfLoop()
+        {
+            Graph graph = new Graph(1);
+            graph.AddEdge(0, 0);
+
+            Assert.True(CycleChecker.HasCycle(graph));
+        }
+
+        [Fact]
+        public void Test_Has_ParallelEdge()
+        {
+            Graph graph = new Graph(2);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 1);
+
+            Assert.True(CycleChecker.HasCycle(graph));
+        }
+
+        [Fact]
+        public void Test_NoCycle()
+        {
+            Graph graph = new Graph(3);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(1, 2);
+
+            Assert.False(CycleChecker.HasCycle(graph));
+        }
+    }
+}
---
+++ Algorithms/Graphs/CycleChecker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Algorithms.Graphs
+{
+    public sealed class CycleChecker
+    {
+        public static Boolean HasCycle(Graph graph)
+        {
+            Boolean[] marked = new Boolean[graph.VerticesCount];
+            Boolean hasCycle = false;
+
+            for (int i = 0; i < graph.VerticesCount; i++)
+                if (!marked[i])
+                    if (CheckCycle(i, i))
+                        break;
+
+            return hasCycle;
+
+            Boolean CheckCycle(Int32 vertice, Int32 parentVertice)
+            {
+                marked[vertice] = true;
+
+                using (var adjacents = graph.GetAdjacentVertices(vertice))
+                {
+                    while (!hasCycle && adjacents.MoveNext())
+                    {
+                        if (adjacents.Current == vertice) // Self-loop: my adjacent is me.
+                            hasCycle = true;
+                        else if (!marked[adjacents.Current])
+                            CheckCycle(adjacents.Current, vertice);
+                        else if (adjacents.Current != parentVertice) // Back-edge: if it's visited and not my parent so it looped to a vertice.
+                            hasCycle = true;
+                    }
+                }
+
+                return hasCycle;
+            }
+        }
+    }
+}
