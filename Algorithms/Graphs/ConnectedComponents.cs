d7567e98d6f0768e14fd60d25c76e5b56ac5dac4
gfurtadoalmeida <gfurtadoalmeida@gmail.com>
2017-12-22 22:52:53 +0000

    New graph element: ConnectedComponents

---
+++ Algorithms/Graphs/ConnectedComponents.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Algorithms.Graphs
+{
+    public sealed class ConnectedComponents
+    {
+        private Boolean[] _connected;
+        private Int32[] _ids;
+
+        /// <summary>
+        /// How many componentes are.
+        /// Being a component an isolated web of connected vertices.
+        /// </summary>
+        public Int32 Count { get; private set; }
+
+        public static ConnectedComponents Create(Graph graph)
+        {
+            return new ConnectedComponents(graph);
+        }
+
+        private ConnectedComponents(Graph graph)
+        {
+            this._connected = new Boolean[graph.VerticesCount];
+            this._ids = new Int32[graph.VerticesCount];
+
+            // How does it work?
+            // For each vertice:
+            //   - If the vertice is not already marked we:
+            //     - Set its id to the current count.
+            //     - Check recursively all its adjacents vertices setting their
+            //       ids to the current count.
+            //   - Increment count.
+
+            // Even though the property "Count" count how many connected componentes
+            // we have it is used to hold the current id while processing.
+
+            for (int i = 0; i < graph.VerticesCount; i++)
+                if (!this._connected[i])
+                {
+                    this.DFS(graph, i);
+
+                    this.Count++;
+                }
+        }
+
+        public Boolean IsConnected(Int32 sourceVertice, Int32 targetVertice)
+        {
+            return this._ids[sourceVertice] == this._ids[targetVertice];
+        }
+
+        public Int32 Id(Int32 vertice)
+        {
+            return this._ids[vertice];
+        }
+
+        private void DFS(Graph graph, Int32 vertice)
+        {
+            this._connected[vertice] = true;
+            this._ids[vertice] = this.Count;
+
+            using (var adjacents = graph.GetAdjacentVertices(vertice))
+            {
+                while (adjacents.MoveNext())
+                {
+                    if (!this._connected[adjacents.Current])
+                        this.DFS(graph, adjacents.Current);
+                }
+            }
+        }
+    }
+}
