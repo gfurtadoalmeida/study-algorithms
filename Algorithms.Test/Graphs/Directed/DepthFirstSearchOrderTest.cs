cdb488eca66c2955c548536b7bc124fa16a50470
gfurtadoalmeida <gfurtadoalmeida@gmail.com>
2018-01-05 23:08:28 +0000

    New digraph component: DepthFirstSearchOrder

---
+++ Algorithms.Test/Graphs/Directed/DepthFirstSearchOrderTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Algorithms.Graphs.Directed;
+using Xunit;
+
+namespace Algorithms.Test.Graphs.Directed
+{
+    public sealed class DepthFirstSearchOrderTest
+    {
+        private static readonly Int32[] PRE_ORDER = new Int32[6] { 0, 3, 4, 5, 1, 2 };
+        private static readonly Int32[] POST_ORDER = new Int32[6] { 5, 4, 3, 2, 1, 0 };
+        private static readonly Int32[] REVERSE_POST_ORDER = new Int32[6] { 0, 1, 2, 3, 4, 5 };
+
+        [Fact]
+        public void Test_Pre()
+        {
+            DepthFirstSearchOrder order = new DepthFirstSearchOrder(this.CreateDigraph(), DepthFirstSearchOrderType.Pre);
+
+            AssertExtensions.Sequence(PRE_ORDER, order.Pre);
+        }
+
+        [Fact]
+        public void Test_Post()
+        {
+            DepthFirstSearchOrder order = new DepthFirstSearchOrder(this.CreateDigraph(), DepthFirstSearchOrderType.Post);
+
+            AssertExtensions.Sequence(POST_ORDER, order.Post);
+        }
+
+        [Fact]
+        public void Test_ReversePost()
+        {
+            DepthFirstSearchOrder order = new DepthFirstSearchOrder(this.CreateDigraph(), DepthFirstSearchOrderType.ReversePost);
+
+            AssertExtensions.Sequence(REVERSE_POST_ORDER, order.ReversePost);
+        }
+
+        private Digraph CreateDigraph()
+        {
+            Digraph graph = new Digraph(6);
+
+            //        0
+            //       / \ 
+            //      3   1 
+            //     /     \
+            //   4        2
+            //  /
+            // 5
+
+            graph.AddEdge(0, 1);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(0, 3);
+            graph.AddEdge(3, 4);
+            graph.AddEdge(4, 5);
+
+            return graph;
+        }
+    }
+}
---
+++ Algorithms/Graphs/Directed/DepthFirstSearchOrder.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using AST = Algorithms.Structures;
+
+namespace Algorithms.Graphs.Directed
+{
+    [Flags]
+    public enum DepthFirstSearchOrderType : byte
+    {
+        /// <summary>
+        /// Normal ordering.
+        /// </summary>
+        Pre = 1,
+
+        /// <summary>
+        /// Reverse ordering.
+        /// </summary>
+        Post = 1 << 1,
+
+        /// <summary>
+        /// Normal ordering but reading from left to righ.
+        /// </summary>
+        ReversePost = 1 << 2
+    }
+
+    public sealed class DepthFirstSearchOrder
+    {
+        private readonly Boolean[] _marked;
+        private readonly AST.Queue<Int32> _pre;
+        private readonly AST.Queue<Int32> _post;
+        private readonly AST.Stack<Int32> _reversePost;
+
+        public DepthFirstSearchOrderType OrderType { get; }
+
+        public IEnumerable<Int32> Pre => this._pre;
+
+        public IEnumerable<Int32> Post => this._post;
+
+        public IEnumerable<Int32> ReversePost => this._reversePost;
+
+        public DepthFirstSearchOrder(Digraph digraph, DepthFirstSearchOrderType orderType)
+        {
+            this.OrderType = orderType;
+
+            this._pre = (orderType & DepthFirstSearchOrderType.Pre) == DepthFirstSearchOrderType.Pre ? new AST.Queue<Int32>() : null;
+            this._post = (orderType & DepthFirstSearchOrderType.Post) == DepthFirstSearchOrderType.Post ? new AST.Queue<Int32>() : null;
+            this._reversePost = (orderType & DepthFirstSearchOrderType.ReversePost) == DepthFirstSearchOrderType.ReversePost ? new AST.Stack<Int32>() : null;
+
+            this._marked = new Boolean[digraph.VerticesCount];
+
+            for (int i = 0; i < digraph.VerticesCount; i++)
+            {
+                if (!this._marked[i])
+                    this.DFS(digraph, i);
+            }
+        }
+
+        private void DFS(Digraph digraph, Int32 vertice)
+        {
+            this._pre?.Enqueue(vertice);
+
+            this._marked[vertice] = true;
+
+            foreach (Int32 w in digraph.GetAdjacentVertices(vertice))
+            {
+                if (!this._marked[w])
+                    this.DFS(digraph, w);
+            }
+
+            this._post?.Enqueue(vertice);
+            this._reversePost?.Push(vertice);
+        }
+    }
+}
