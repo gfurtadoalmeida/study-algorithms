bbfa9281918dc0e2491592539b70c45b5af486c9
gfurtadoalmeida <gfurtadoalmeida@gmail.com>
2018-01-05 21:46:06 +0000

    New graph component: Digraph CycleChecker

---
+++ Algorithms.Test/Graphs/Directed/CycleCheckerTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Algorithms.Graphs.Directed;
+using Xunit;
+
+namespace Algorithms.Test.Graphs.Directed
+{
+    public sealed class CycleCheckerTest
+    {
+        [Fact]
+        public void Test_NoCycle()
+        {
+            Digraph graph = new Digraph(3);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 2);
+
+            CycleChecker checker = new CycleChecker(graph);
+
+            Assert.False(checker.HasCycle);
+        }
+
+        [Fact]
+        public void Test_HasCycle()
+        {
+            Digraph graph = new Digraph(3);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(2, 0);
+
+            CycleChecker checker = new CycleChecker(graph);
+
+            Assert.True(checker.HasCycle);
+        }
+
+        [Fact]
+        public void Test_GetCycle()
+        {
+            Digraph graph = new Digraph(3);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(2, 0);
+
+            CycleChecker checker = new CycleChecker(graph);
+
+            using (IEnumerator<Int32> enumerator = checker.GetCycle().GetEnumerator())
+            {
+                enumerator.MoveNext();
+
+                Assert.Equal(2, enumerator.Current);
+
+                enumerator.MoveNext();
+
+                Assert.Equal(0, enumerator.Current);
+
+                enumerator.MoveNext();
+
+                Assert.Equal(1, enumerator.Current);
+
+                enumerator.MoveNext();
+
+                Assert.Equal(2, enumerator.Current);
+
+                Assert.False(enumerator.MoveNext());
+            }
+        }
+    }
+}
---
+++ Algorithms/Graphs/Directed/CycleChecker.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using AST = Algorithms.Structures;
+
+namespace Algorithms.Graphs.Directed
+{
+    public sealed class CycleChecker
+    {
+        // Mark if the vertice was visited.
+        private readonly Boolean[] _visited;
+        // Vertices on the stack of "vertices being processed".
+        private readonly Boolean[] _onStack;
+        // Parent vertice of "_edgeto[vertice]".
+        private readonly Int32[] _edgeTo;
+
+        // Vertices on a cycle, from the end to start.
+        private AST.Stack<Int32> _cycle;
+
+        public Boolean HasCycle => this._cycle != null;
+
+        public CycleChecker(Digraph digraph)
+        {
+            if (digraph == null)
+                throw new ArgumentNullException(nameof(digraph));
+
+            this._visited = new Boolean[digraph.VerticesCount];
+            this._onStack = new Boolean[digraph.VerticesCount];
+            this._edgeTo = new Int32[digraph.VerticesCount];
+
+            for (int i = 0; i < digraph.VerticesCount; i++)
+            {
+                if (!this._visited[i])
+                    this.DFS(digraph, i);
+            }
+        }
+
+        public IEnumerable<Int32> GetCycle()
+        {
+            return this._cycle;
+        }
+
+        private void DFS(Digraph digraph, Int32 vertice)
+        {
+            this._onStack[vertice] = true;
+            this._visited[vertice] = true;
+
+            foreach (Int32 adjacent in digraph.GetAdjacentVertices(vertice))
+            {
+                if (this.HasCycle)
+                    return;
+
+                // The cycle occurrs when an adjacent vertice is on the stack of 
+                // "vertices being processed".
+                // If vertice A is being processed (onStack) and one of its adjacents
+                // points to A again, we have a cycle.
+
+                if (!this._visited[adjacent])
+                {
+                    this._edgeTo[adjacent] = vertice;
+
+                    this.DFS(digraph, adjacent);
+                }
+                else if (this._onStack[adjacent])
+                {
+                    this._cycle = new AST.Stack<Int32>();
+
+                    for (int i = vertice; i != adjacent; i = this._edgeTo[i])
+                        this._cycle.Push(i);
+
+                    this._cycle.Push(adjacent);
+                    this._cycle.Push(vertice);
+                }
+            }
+            
+            // If we fully processed a vertice, it means the vertice is no more on
+            // the stack.
+
+            this._onStack[vertice] = false;
+        }
+    }
+}
