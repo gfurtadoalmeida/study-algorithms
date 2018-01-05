0ae5165626434e754b43ee219daf2bc0a0d06cf1
gfurtadoalmeida <gfurtadoalmeida@gmail.com>
2018-01-05 23:56:25 +0000

    New digraph component: TopologicalChecker

---
+++ Algorithms.Test/Graphs/Directed/TopologicalCheckerTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Algorithms.Graphs.Directed;
+using Xunit;
+
+namespace Algorithms.Test.Graphs.Directed
+{
+    public sealed class TopologicalCheckerTest
+    {
+        [Fact]
+        public void Test_IsNotDAG()
+        {
+            Digraph graph = new Digraph(3);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(2, 0);
+
+            TopologicalChecker checker = new TopologicalChecker(graph);
+
+            Assert.False(checker.IsDAG);
+        }
+
+        [Fact]
+        public void Test_IsDAG()
+        {
+            Digraph graph = new Digraph(3);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 2);
+
+            TopologicalChecker checker = new TopologicalChecker(graph);
+
+            Assert.True(checker.IsDAG);
+        }
+
+        [Fact]
+        public void Test_Order()
+        {
+            Digraph graph = new Digraph(4);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 2);
+            graph.AddEdge(2, 3);
+
+            TopologicalChecker checker = new TopologicalChecker(graph);
+
+            AssertUtilities.Sequence(new Int32[4] { 0, 1, 2, 3 }, checker.Order);
+        }
+    }
+}
---
+++ Algorithms/Graphs/Directed/TopologicalChecker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Graphs.Directed
+{
+    public sealed class TopologicalChecker
+    {
+        public IEnumerable<Int32> Order { get; }
+
+        public Boolean IsDAG => this.Order != null;
+
+        public TopologicalChecker(Digraph digraph)
+        {
+            CycleChecker checker = new CycleChecker(digraph);
+
+            if (!checker.HasCycle)
+            {
+                DepthFirstSearchOrder dfs = new DepthFirstSearchOrder(digraph, DepthFirstSearchOrderType.ReversePost);
+
+                this.Order = dfs.ReversePost;
+            }
+        }
+    }
+}
