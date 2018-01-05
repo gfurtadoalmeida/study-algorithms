71d62e6e0af0f22ba803184e723d4f47aac62560
gfurtadoalmeida <gfurtadoalmeida@gmail.com>
2018-01-05 23:08:06 +0000

    New assertion utility

---
+++ Algorithms.Test/AssertExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Algorithms.Test
+{
+    public static class AssertExtensions
+    {
+        public static void Sequence<T>(IEnumerable<T> expected, IEnumerable<T> actual)
+        {
+            using (IEnumerator<T> enumeratorActual = actual.GetEnumerator())
+            using (IEnumerator<T> enumeratorExpected = expected.GetEnumerator())
+            {
+                while (enumeratorActual.MoveNext())
+                {
+                    if (!enumeratorExpected.MoveNext())
+                        throw new SequenceSizeException();
+
+                    Assert.Equal(enumeratorExpected.Current, enumeratorActual.Current);
+                }
+
+                if (enumeratorExpected.MoveNext())
+                    throw new SequenceSizeException();
+            }
+        }
+    }
+
+    public sealed class SequenceSizeException : Xunit.Sdk.XunitException
+    {
+        public SequenceSizeException() : base("IEnumerable 'expected' has a different size than IEnumerable 'value'.")
+        {
+        }
+    }
+}
