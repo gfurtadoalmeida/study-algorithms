0e91276a39241d19a88c35944b6560c58b319d3f
gfurtadoalmeida <gfurtadoalmeida@gmail.com>
2018-01-06 00:57:48 +0000

    New digraph component: KosarajuSharirSCC

---
+++ Algorithms.Test/Graphs/Directed/KosarajuSharirSCCTest.cs
@@ -0,0 +1,55 @@
+﻿using Algorithms.Graphs.Directed;
+using Xunit;
+
+namespace Algorithms.Test.Graphs.Directed
+{
+    public sealed class KosarajuSharirSCCTest
+    {
+        [Fact]
+        public void Test_Count()
+        {
+            KosarajuSharirSCC scc = this.CreateSCC();
+
+            Assert.Equal(4, scc.Count);
+        }
+
+        [Fact]
+        public void Test_IsStronglyConnected()
+        {
+            KosarajuSharirSCC scc = this.CreateSCC();
+
+            Assert.True(scc.IsStronglyConnected(0, 1));
+        }
+
+        [Fact]
+        public void Test_NotStronglyConnected()
+        {
+            KosarajuSharirSCC scc = this.CreateSCC();
+
+            Assert.False(scc.IsStronglyConnected(0, 4));
+        }
+
+        [Fact]
+        public void Test_Id()
+        {
+            KosarajuSharirSCC scc = this.CreateSCC();
+
+            Assert.Equal(3, scc.Id(0));
+            Assert.Equal(1, scc.Id(4));
+        }
+
+        private KosarajuSharirSCC CreateSCC()
+        {
+            Digraph graph = new Digraph(7);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(2, 0);
+            graph.AddEdge(3, 5);
+            graph.AddEdge(4, 5);
+            graph.AddEdge(6, 4);
+            graph.AddEdge(4, 6);
+
+            return KosarajuSharirSCC.Create(graph);
+        }
+    }
+}
---
+++ Algorithms/Graphs/Directed/KosarajuSharirSCC.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Algorithms.Graphs.Directed
+{
+    /// <summary>
+    /// Finds how many strongly connected compopnentes are in a digraph.
+    /// </summary>
+    public sealed class KosarajuSharirSCC
+    {
+        private Boolean[] _visited;
+        private Int32[] _ids;
+
+        /// <summary>
+        /// How many componentes.
+        /// Being a component an isolated web of strongly connected vertices or a sole vertice.
+        /// </summary>
+        public Int32 Count { get; private set; }
+
+        public static KosarajuSharirSCC  Create(Digraph digraph)
+        {
+            return new KosarajuSharirSCC(digraph);
+        }
+
+        private KosarajuSharirSCC(Digraph digraph)
+        {
+            DepthFirstOrder dfs = new DepthFirstOrder(digraph.Reverse(), DepthFirstOrderType.ReversePost);
+
+            this._visited = new Boolean[digraph.VerticesCount];
+            this._ids = new Int32[digraph.VerticesCount];
+
+            foreach (Int32 vertice in dfs.ReversePost)
+            {
+                if (!this._visited[vertice])
+                {
+                    this.DFS(digraph, vertice);
+
+                    this.Count++;
+                }
+            }
+        }
+
+        public Boolean IsStronglyConnected(Int32 sourceVertice, Int32 targetVertice)
+        {
+            return this._ids[sourceVertice] == this._ids[targetVertice];
+        }
+
+        public Int32 Id(Int32 vertice)
+        {
+            return this._ids[vertice];
+        }
+
+        private void DFS(Digraph digraph, Int32 vertice)
+        {
+            this._visited[vertice] = true;
+            this._ids[vertice] = this.Count;
+
+            foreach (Int32 adjacent in digraph.GetAdjacentVertices(vertice))
+            {
+                if (!this._visited[adjacent])
+                    this.DFS(digraph, adjacent);
+            }
+        }
+    }
+}
