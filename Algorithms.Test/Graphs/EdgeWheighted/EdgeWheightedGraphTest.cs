9e0da7fe9ede3da8d46b49b04b3544e52999fe79
gfurtadoalmeida <gfurtadoalmeida@gmail.com>
2018-01-06 20:34:09 +0000

    New graph type: EdgeWheightedGraph

---
+++ Algorithms.Test/Graphs/EdgeWheighted/EdgeWheightedGraphTest.cs
@@ -0,0 +1,68 @@
+﻿using Algorithms.Graphs.EdgeWheighted;
+using Xunit;
+
+namespace Algorithms.Test.Graphs.EdgeWheighted
+{
+    public sealed class EdgeWheightedGraphTest
+    {
+        [Fact]
+        public void Test_EdgesCount_OnCreation()
+        {
+            EdgeWheightedGraph graph = new EdgeWheightedGraph(1);
+
+            Assert.Equal(0, graph.EdgesCount);
+        }
+
+        [Fact]
+        public void Test_VerticesCount_OnCreation()
+        {
+            EdgeWheightedGraph graph = new EdgeWheightedGraph(1);
+
+            Assert.Equal(1, graph.VerticesCount);
+        }
+
+        [Fact]
+        public void Test_Counts_WhenAdding()
+        {
+            EdgeWheightedGraph graph = new EdgeWheightedGraph(3);
+            graph.AddEdge(new Edge(0, 1, 0.5));
+            graph.AddEdge(new Edge(1, 2, 0.2));
+            graph.AddEdge(new Edge(2, 0, 0.2));
+
+            Assert.Equal(3, graph.EdgesCount);
+            Assert.Equal(3, graph.VerticesCount);
+        }
+
+        [Fact]
+        public void Test_Iteration()
+        {
+            EdgeWheightedGraph graph = new EdgeWheightedGraph(3);
+
+            Edge e0 = new Edge(0, 1, 0.5);
+            Edge e1 = new Edge(1, 2, 0.2);
+            Edge e2 = new Edge(2, 0, 0.1);
+
+            graph.AddEdge(e0);
+            graph.AddEdge(e1);
+            graph.AddEdge(e2);
+
+            AssertUtilities.Sequence(new Edge[2] { e2, e0 }, graph.GetAdjacentVertices(0));
+        }
+
+        [Fact]
+        public void Test_GetEdges_Iteration()
+        {
+            EdgeWheightedGraph graph = new EdgeWheightedGraph(3);
+
+            Edge e0 = new Edge(0, 1, .5);
+            Edge e1 = new Edge(1, 2, 0.2);
+            Edge e2 = new Edge(2, 0, 0.1);
+
+            graph.AddEdge(e0);
+            graph.AddEdge(e1);
+            graph.AddEdge(e2);
+
+            AssertUtilities.Sequence(new Edge[3] { e1, e0, e2 }, graph.GetEdges());
+        }
+    }
+}
---
+++ Algorithms/Graphs/EdgeWheighted/EdgeWheightedGraph.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Algorithms.Structures;
+
+namespace Algorithms.Graphs.EdgeWheighted
+{
+    /// <summary>
+    /// Used on connectivity problems where edges have wheight.
+    /// v - w
+    /// </summary>
+    public sealed class EdgeWheightedGraph
+    {
+        private readonly Bag<Edge>[] _adjacencyVertices;
+
+        public Int32 VerticesCount { get; private set; }
+
+        public Int32 EdgesCount { get; private set; }
+
+        public EdgeWheightedGraph(Int32 verticesCount)
+        {
+            this.VerticesCount = verticesCount;
+            this.EdgesCount = 0;
+
+            this._adjacencyVertices = new Bag<Edge>[verticesCount];
+
+            for (int i = 0; i < verticesCount; i++)
+                this._adjacencyVertices[i] = new Bag<Edge>();
+        }
+
+        public void AddEdge(Edge edge)
+        {
+            this.ThrowIfVerticeIndexOutOfRange(nameof(edge) + "." + nameof(edge.Source), edge.Source);
+            this.ThrowIfVerticeIndexOutOfRange(nameof(edge) + "." + nameof(edge.Target), edge.Target);
+
+            this._adjacencyVertices[edge.Source].Add(edge);
+            this._adjacencyVertices[edge.Target].Add(edge);
+
+            this.EdgesCount++;
+        }
+
+        public IEnumerable<Edge> GetAdjacentVertices(Int32 verticeIndex)
+        {
+            this.ThrowIfVerticeIndexOutOfRange(nameof(verticeIndex), verticeIndex);
+
+            return this._adjacencyVertices[verticeIndex];
+        }
+
+        public IEnumerable<Edge> GetEdges()
+        {
+            Bag<Edge> bag = new Bag<Edge>();
+
+            for (int i = 0; i < this.VerticesCount; i++)
+            {
+                foreach (Edge edge in this.GetAdjacentVertices(i))
+                {
+                    if (edge.Other(i) > i)
+                        bag.Add(edge);
+                }
+            }
+
+            return bag;
+        }
+
+        private void ThrowIfVerticeIndexOutOfRange(String parameterName, Int32 verticeIndex)
+        {
+            if (verticeIndex < 0 || verticeIndex > this._adjacencyVertices.Length - 1)
+                throw new ArgumentException(parameterName,
+                                            "Value must be greater or equal to zero and less than " + this.VerticesCount);
+        }
+    }
+}
