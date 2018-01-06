2827dec0b1354df87b762502dcf85c2b7176bed8
gfurtadoalmeida <gfurtadoalmeida@gmail.com>
2018-01-06 19:09:09 +0000

    New digraph component: TransitiveClosure

---
+++ Algorithms.Test/Graphs/Directed/TransitiveClosureTest.cs
@@ -0,0 +1,33 @@
+﻿using Algorithms.Graphs.Directed;
+using Xunit;
+
+namespace Algorithms.Test.Graphs.Directed
+{
+    public sealed class TransitiveClosureTest
+    {
+        [Fact]
+        public void Test_IsReachable()
+        {
+            Digraph graph = new Digraph(3);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 2);
+
+            TransitiveClosure closure = TransitiveClosure.Create(graph);
+
+            Assert.True(closure.IsReachable(0,2));
+        }
+
+        [Fact]
+        public void Test_NotReachable()
+        {
+            Digraph graph = new Digraph(4);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(0, 3);
+
+            TransitiveClosure closure = TransitiveClosure.Create(graph);
+
+            Assert.False(closure.IsReachable(3, 1));
+        }
+    }
+}
---
+++ Algorithms/Graphs/Directed/TransitiveClosure.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Algorithms.Graphs.Directed
+{
+    public sealed class TransitiveClosure
+    {
+        private DirectedDepthFirstSearch[] _all;
+
+        public static TransitiveClosure Create(Digraph digraph)
+        {
+            if (digraph == null)
+                throw new ArgumentNullException(nameof(digraph));
+
+            return new TransitiveClosure(digraph);
+        }
+
+        private TransitiveClosure(Digraph digraph)
+        {
+            this._all = new DirectedDepthFirstSearch[digraph.VerticesCount];
+
+            for (int i = 0; i < digraph.VerticesCount; i++)
+                this._all[i] = new DirectedDepthFirstSearch(digraph, i);
+        }
+
+        public Boolean IsReachable(Int32 sourceVertice, Int32 targetVertice)
+        {
+            return this._all[sourceVertice].HasDirectPathTo(targetVertice);
+        }
+    }
+}
