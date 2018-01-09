1dd1351b8cfddaf02f87bac5ba750f636206c489
gfurtadoalmeida <gfurtadoalmeida@gmail.com>
2018-01-09 23:17:57 +0000

    New sorting algorithm: KeyIndexedCountingSort

---
+++ Algorithms.Test/Sorting/KeyIndexedCountingSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Algorithms.Sorting;
+using Xunit;
+
+namespace Algorithms.Test.Sorting
+{
+    public sealed class KeyIndexedCountingSortTest
+    {
+        [Fact]
+        public void Test()
+        {
+            KeyValuePair<Int32, Double> kv1 = new KeyValuePair<Int32, Double>(1, 0.5);
+            KeyValuePair<Int32, Double> kv2 = new KeyValuePair<Int32, Double>(1, 0.8);
+            KeyValuePair<Int32, Double> kv3 = new KeyValuePair<Int32, Double>(2, 1.8);
+            KeyValuePair<Int32, Double> kv4 = new KeyValuePair<Int32, Double>(2, 1.2);
+            KeyValuePair<Int32, Double> kv5 = new KeyValuePair<Int32, Double>(3, 2.2);
+            KeyValuePair<Int32, Double> kv6 = new KeyValuePair<Int32, Double>(3, 2.2);
+
+            KeyValuePair<Int32, Double>[] sorted = new KeyValuePair<Int32, Double>[6]
+            {
+                kv1, kv2, kv3, kv4, kv5, kv6
+            };
+
+            KeyValuePair<Int32, Double>[] notSorted = new KeyValuePair<Int32, Double>[6]
+            {
+                kv1, kv5, kv2, kv3, kv6, kv4
+            };
+
+            KeyIndexedCountingSort.Sort(notSorted, 4);
+
+            AssertUtilities.Sequence(sorted, notSorted);
+        }
+    }
+}
---
+++ Algorithms/Sorting/KeyIndexedCountingSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Sorting
+{
+    public static class KeyIndexedCountingSort
+    {
+        /// <summary>
+        /// Sorts an array of key-value pair by key, where key is between 0 and radix - 1.
+        /// </summary>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="itens">Array of key-value pair.</param>
+        /// <param name="radix">Number of unique digits, including zero.</param>
+        public static void Sort<TValue>(KeyValuePair<Int32, TValue>[] itens, Int32 radix)
+        {
+            KeyValuePair<Int32, TValue>[] aux = new KeyValuePair<Int32, TValue>[itens.Length];
+            Int32[] frequency = new Int32[radix + 1];
+
+            // 1 - Count frequencies of each iten using key as index.
+
+            for (int i = 0; i < itens.Length; i++)
+                frequency[itens[i].Key + 1]++;
+
+            // 2 - Compute frequency cumulates which specify destinations.
+
+            for (int i = 0; i < radix; i++)
+                frequency[i + 1] += frequency[i];
+
+            // 3 - Access cumulates using key as index to move items.
+
+            for (int i = 0; i < itens.Length; i++)
+                aux[frequency[itens[i].Key]++] = itens[i];
+
+            // 4 - Copy back into original array.
+
+            for (int i = 0; i < itens.Length; i++)
+                itens[i] = aux[i];
+        }
+    }
+}
