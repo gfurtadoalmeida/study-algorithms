b9f63d50d347e1c939106b840d1097e7271006e0
gfurtadoalmeida <gfurtadoalmeida@gmail.com>
2018-01-10 23:23:54 +0000

    Show the expected and actual count

--- Algorithms.Test/AssertUtilities.cs
+++ Algorithms.Test/AssertUtilities.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Algorithms.Test
@@ -7,26 +8,46 @@ namespace Algorithms.Test
     {
         public static void Sequence<T>(IEnumerable<T> expected, IEnumerable<T> actual)
         {
+            Int32 countExpected = 0;
+            Int32 countActual = 0;
+
             using (IEnumerator<T> enumeratorActual = actual.GetEnumerator())
             using (IEnumerator<T> enumeratorExpected = expected.GetEnumerator())
             {
                 while (enumeratorActual.MoveNext())
                 {
+                    countExpected++;
+                    countActual++;
+
                     if (!enumeratorExpected.MoveNext())
-                        throw new SequenceSizeException();
+                    {
+                        // Unroll to count.
+                        while (enumeratorActual.MoveNext())
+                            countActual++;
+
+                        throw new SequenceSizeException(countExpected, countActual);
+                    }
 
                     Assert.Equal(enumeratorExpected.Current, enumeratorActual.Current);
                 }
 
                 if (enumeratorExpected.MoveNext())
-                    throw new SequenceSizeException();
+                {
+                    countExpected++;
+
+                    // Unroll to count.
+                    while (enumeratorExpected.MoveNext())
+                        countExpected++;
+
+                    throw new SequenceSizeException(countExpected, countActual);
+                }
             }
         }
     }
 
     public sealed class SequenceSizeException : Xunit.Sdk.XunitException
     {
-        public SequenceSizeException() : base("IEnumerable 'expected' has a different size than IEnumerable 'value'.")
+        public SequenceSizeException(Int32 countExpected, Int32 countActual) : base($"Sequence count not equal. Expected: {countExpected} | Actual: {countActual}.")
         {
         }
     }
