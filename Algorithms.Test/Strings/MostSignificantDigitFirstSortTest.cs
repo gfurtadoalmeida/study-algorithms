b6de34156464b8bfb4f12bb66c8caa13153c5a93
gfurtadoalmeida <gfurtadoalmeida@gmail.com>
2018-01-10 20:44:20 +0000

    New string algorithm: MostSignificantDigitFirstSort

---
+++ Algorithms.Test/Strings/MostSignificantDigitFirstSortTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Algorithms.Strings;
+using Xunit;
+
+namespace Algorithms.Test.Strings
+{
+    public sealed class MostSignificantDigitFirstSortTest
+    {
+        [Fact]
+        public void Test_ToChar()
+        {
+            String s1 = "100";
+            String s2 = "10021";
+            String s3 = "201341";
+            String s4 = "201442";
+            String s5 = "3015";
+            String s6 = "400";
+            String s7 = "40021";
+            String s8 = "501341";
+            String s9 = "501442";
+            String s10 = "6015";
+            String s11 = "700";
+            String s12 = "70021";
+            String s13 = "801341";
+            String s14 = "801442";
+            String s15 = "9015";
+            String s16 = "9016";
+
+            String[] sorted = new String[16] { s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15, s16 };
+            String[] unsorted = new String[16] { s8, s5, s9, s15, s3, s4, s13, s1, s11, s2, s7, s16, s10, s12, s14, s6 };
+
+            MostSignificantDigitFirstSort.Sort(unsorted, Alphabet.Decimal);
+
+            AssertUtilities.Sequence(sorted, unsorted);
+        }
+    }
+}
---
+++ Algorithms/Strings/MostSignificantDigitFirstSort.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Algorithms.Structures;
+
+namespace Algorithms.Strings
+{
+    /// <summary>
+    /// String sorting where the strings have different lengths.
+    /// Uses key-indexed counting sort reading left to right.
+    /// </summary>
+    public static class MostSignificantDigitFirstSort
+    {
+        private static Int32 CUT_OFF = 15; // Cutoff for small subarrays.
+
+        public static void Sort(String[] array, Alphabet alphabet)
+        {
+            String[] aux = new String[array.Length];
+
+            Sort(array, aux, 0, array.Length - 1, 0, alphabet);
+        }
+
+        private static void Sort(String[] array,
+            String[] aux,
+            Int32 low,
+            Int32 high,
+            Int32 index,
+            Alphabet alphabet)
+        {
+            if (high <= low + CUT_OFF)
+            {
+                InsertionSort(array, low, high, index);
+
+                return;
+            }
+
+            CompactBoundedArray<Int32> count = new CompactBoundedArray<Int32>(alphabet.MinChar, alphabet.MaxChar + 2);
+
+            // 1 - Compute frequency counts.
+
+            for (int i = low; i <= high; i++)
+                count[CharAt(array[i], index) + 2]++;
+
+            // 2 - Transform counts to indices.
+
+            for (int i = 0; i < alphabet.Radix + 1; i++)
+                count[i + 1] += count[i];
+
+            // 4 - Distribute.
+
+            for (int i = low; i <= high; i++)
+                aux[count[CharAt(array[i], index) + 1]++] = array[i];
+
+            // 5 - Copy back.
+
+            for (int i = low; i <= high; i++)
+                array[i] = aux[i - low];
+
+            // 6 - Recursively sort for each character value.
+
+            for (int i = 0; i < alphabet.Radix; i++)
+                Sort(array, 
+                     aux, 
+                     low + count[i], 
+                     low + count[i + 1] - 1, 
+                     index + 1, 
+                     alphabet);
+        }
+
+        private static Int32 CharAt(String text, Int32 index)
+        {
+            if (index < text.Length)
+                return text[index];
+
+            return -1;
+        }
+
+        private static void InsertionSort(String[] array, Int32 low, Int32 high, Int32 index)
+        {
+            for (int i = low; i <= high; i++)
+                for (int j = i; j > low && IsLess(array[j], array[j - 1], index); j--)
+                    Exchange(array, j, j - 1);
+        }
+
+        private static void Exchange(String[] array, Int32 indexSource, Int32 indexTarget)
+        {
+            String temp = array[indexSource];
+            array[indexSource] = array[indexTarget];
+            array[indexTarget] = temp;
+        }
+
+        private static Boolean IsLess(String source, String target, Int32 index)
+        {
+            for (int i = index; i < Math.Min(source.Length, target.Length); i++)
+            {
+                if (source[i] < target[i])
+                    return true;
+
+                if (source[i] > target[i])
+                    return false;
+            }
+
+            return source.Length < target.Length;
+        }
+    }
+}
