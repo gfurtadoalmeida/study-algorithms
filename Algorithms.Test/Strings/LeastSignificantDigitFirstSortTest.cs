016944cd224dc225597c0beecd94bda66098098c
gfurtadoalmeida <gfurtadoalmeida@gmail.com>
2018-01-10 14:40:22 +0000

    New strings algorithm: LeastSignificantDigitFirstSort

---
+++ Algorithms.Test/Strings/LeastSignificantDigitFirstSortTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Algorithms.Strings;
+using Xunit;
+
+namespace Algorithms.Test.Strings
+{
+    public sealed class LeastSignificantDigitFirstSortTest
+    {
+        [Fact]
+        public void Test_ToChar()
+        {
+            String s1 = "1001";
+            String s2 = "1002";
+            String s3 = "2013";
+            String s4 = "2014";
+            String s5 = "3015";
+
+            String[] sorted = new String[5] { s1, s2, s3, s4, s5 };
+            String[] unsorted = new String[5] { s5, s3, s4, s1, s2 };
+
+            LeastSignificantDigitFirstSort.Sort(unsorted, 4, Alphabet.Decimal);
+
+            AssertUtilities.Sequence(sorted, unsorted);
+        }
+    }
+}
---
+++ Algorithms/Strings/LeastSignificantDigitFirstSort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Algorithms.Structures;
+
+namespace Algorithms.Strings
+{
+    /// <summary>
+    /// Method of choice for string-sorting applications where all the keys are the same length.
+    /// Uses key-indexed counting sort reading from right to left.
+    /// </summary>
+    public static class LeastSignificantDigitFirstSort
+    {
+        public static void Sort(String[] array, Int32 stringLength, Alphabet alphabet)
+        {
+            String[] aux = new String[array.Length];
+            CompactBoundedArray<Int32> count = new CompactBoundedArray<Int32>(alphabet.MinChar, alphabet.MaxChar);
+
+            // Sort by key-indexed counting on dth character.
+            // Same logic of KeyIndexedCountingSort.
+            // Reading right to left.
+
+            for (int c = stringLength - 1; c >= 0; c--)
+            {
+                // 1 - Compute frequency counts.
+
+                for (int i = 0; i < array.Length; i++)
+                    count[array[i][c] + 1]++;
+
+                // 2 - Compute cumulates.
+
+                for (int i = count.LowerBound; i < count.UpperBound; i++)
+                    count[i + 1] += count[i];
+
+                // 3 - Move data.
+
+                for (int i = 0; i < array.Length; i++)
+                    aux[count[array[i][c]]++] = array[i];
+
+                // 4 - Copy data back.
+
+                for (int i = 0; i < array.Length; i++)
+                    array[i] = aux[i];
+                
+                // Cleaning for the next round.
+
+                for (int i = count.LowerBound; i < count.UpperBound; i++)
+                    count[i] = 0;
+            }
+        }
+    }
+}
