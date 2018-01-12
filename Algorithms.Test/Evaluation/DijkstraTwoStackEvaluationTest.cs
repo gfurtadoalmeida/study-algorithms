a4f891624ad0818072e8581df12d7dfff16cf343
gfurtadoalmeida <gfurtadoalmeida@gmail.com>
2018-01-12 20:50:31 +0000

    New evaluation algorithm: DijkstraTwoStackEvaluation

---
+++ Algorithms.Test/Evaluation/DijkstraTwoStackEvaluationTest.cs
@@ -0,0 +1,14 @@
+﻿using Algorithms.Evaluation;
+using Xunit;
+
+namespace Algorithms.Test.Evaluation
+{
+    public sealed class DijkstraTwoStackEvaluationTest
+    {
+        [Fact]
+        public void Test_Evaluation()
+        {
+            Assert.Equal(-1, DijkstraTwoStackEvaluation.Evaluate("( ( ( 1 + sqrt ( 49 ) ) / ( 2 * 1 ) ) - 5 )"));
+        }
+    }
+}
---
+++ Algorithms/Evaluation/DijkstraTwoStackEvaluation.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using AST = Algorithms.Structures;
+
+namespace Algorithms.Evaluation
+{
+    /// <summary>
+    /// Evaluates arithmetic expressions that are fully parenthesized, with 
+    /// numbers and characters separated by whitespace.
+    /// </summary>
+    public static class DijkstraTwoStackEvaluation
+    {
+        public static Double Evaluate(String expression)
+        {
+            AST.Stack<String> operations = new  AST.Stack<String>();
+            AST.Stack<Double> values = new AST.Stack<Double>();
+
+            String[] tokens = expression.Split(" ");
+
+            foreach (String token in tokens)
+            {
+                switch (token)
+                {
+                    case "(":
+                        break;
+
+                    case "+":
+                    case "-":
+                    case "*":
+                    case "/":
+                    case "sqrt":
+                        operations.Push(token);
+                        break;
+
+                    case ")":
+                        // Pop, evaluate, and push result if token is ")".
+                        String operation = operations.Pop();
+                        Double value = values.Pop();
+
+                        switch (operation)
+                        {
+                            case "+":
+                                value = values.Pop() + value;
+                                break;
+
+                            case "-":
+                                value = values.Pop() - value;
+                                break;
+
+                            case "*":
+                                value = values.Pop() * value;
+                                break;
+
+                            case "/":
+                                value = values.Pop() / value;
+                                break;
+
+                            case "sqrt":
+                                value = Math.Sqrt(value);
+                                break;
+                        }
+
+                        values.Push(value);
+                        break;
+
+                    default:
+                        // If token is not operator or parentheses: push double value.
+                        values.Push(Double.Parse(token));
+                        break;
+                } 
+            }
+
+            return values.Pop();
+        }
+    }
+}
